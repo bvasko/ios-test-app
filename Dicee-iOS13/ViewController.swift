566416935f94e490d456bcf64035321c44818baf
Bonnie DiPasquale <bvasko+github@gmail.com>
2022-04-08 12:41:27 -0400

    update dice images

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,20 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         diceImageView1.image = UIImage(named: "DiceSix")
-        diceImageView1.alpha = 0.2
-    }
-    
-    @IBOutlet weak var diceImageView2: UIImageView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
         diceImageView2.image = UIImage(name: "DiceTwo")
+        diceImageView1.alpha = 0.2
     }
     
     @IBAction func rollButtonPressed(_ sender: UITextField) {
         print("Button got tapped")
+        
+        
     }
 }
