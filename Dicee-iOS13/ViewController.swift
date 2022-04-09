cbb26ca81449b8bc54b67f61b9783a83b2eb37c1
Bonnie DiPasquale <bvasko+github@gmail.com>
2022-04-08 23:24:04 -0400

    generate random dice number

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -11,16 +11,23 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
+    var diceArray = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"), UIImage(named: "DiceSix")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         diceImageView1.image = UIImage(named: "DiceSix")
         diceImageView2.image = UIImage(named: "DiceTwo")
-        diceImageView1.alpha = 0.2
     }
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageView1.image = UIImage(named: "DiceFour")
-        diceImageView2.image = UIImage(named: "DiceFour")
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+        leftDiceNumber += 1
+        rightDiceNumber -= 1
+        print(Int.random(in: 1...10))
     }
 }
